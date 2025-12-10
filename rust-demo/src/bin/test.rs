d37973c7bca47a1e5d8bd4b2d326116f7356c724
carpcap <carpcapx@outlook.com>
2025-12-10 12:07:41 +0800

    rust

--- rust-demo/src/bin/test.rs
+++ rust-demo/src/bin/test.rs
@@ -1,7 +1,8 @@
-fn test() -> i32 {
-    1 + 5
-}
-
 fn main() {
-    println!("test function returns: {}", test());
+    let x = 5;
+    let y = &x;
+
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
