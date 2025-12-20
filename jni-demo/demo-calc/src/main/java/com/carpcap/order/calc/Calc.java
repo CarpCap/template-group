de8177fd44c329d04d97dfabb600dcc729c4534f
carpcap <carpcapx@outlook.com>
2025-12-20 19:14:19 +0800

    1

--- jni-demo/demo-calc/src/main/java/com/carpcap/order/calc/Calc.java
+++ jni-demo/demo-calc/src/main/java/com/carpcap/order/calc/Calc.java
@@ -9,16 +9,24 @@ package com.carpcap.order.calc;
 public class Calc {
 
 
-
     public static native long calcJni(String type, long a1, long a2);
+
+
+    static {
+        String osName = System.getProperty("os.name");
+        if (osName.startsWith("win") || osName.startsWith("Win")) {
+            System.loadLibrary("src/main/resources/calc");
+        } else {
+            // 这里需要提前将 JniDemo.so 放入linux目录/usr/lib 下 并且执行命令sudo ldconfig
+            System.load("/usr/lib/calc.so");
+        }
+
+    }
 
     public static void main(String[] args) {
 
-//        System.out.println(System.getProperty("java.library.path"));
-
-        System.loadLibrary("src/main/java/com/carpcap/order/calc/Calc");
-        System.out.println(calcJni("add", 3, 3));
-        System.out.println(calcJni("mul", 3, 3));
+        System.out.println(calcJni("add", 5, 5));
+        System.out.println(calcJni("mul", 5, 5));
 
     }
 }
